4028ca87fc4e06f7e07d4053f0ec291a65e98bc9
GuiPil <guillaume.pilon@umontreal.ca>
2022-02-08 16:07:46 -0500

    fonction parser revisite

--- TP1/src/src/Parser.java
+++ TP1/src/src/Parser.java
@@ -1,14 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
     Analyser mainAnalyser;
 
+    public Parser(Analyser a) {
+       this.mainAnalyser = a;
+    }
+
     public void parse(File f) throws FileNotFoundException {
         File file = f;
         Scanner scanner = new Scanner(file);
+
+
+
         int line_count = 0;
         int comment_count = 0;
         boolean comment_check = false;
@@ -17,31 +26,23 @@ public class Parser {
             String nextline = scanner.nextLine();
 
 
-            for(int i = 0; i < nextline.length(); i++){
-
-
-                if(i < nextline.length()-2){
-
-                    if((""+nextline.charAt(i)+nextline.charAt(i+1)).equals("//")){
-                        comment_count++;
-                    }
-                    if((""+nextline.charAt(i)+nextline.charAt(i+1)).equals("/*")){
-                        System.out.println("Yeah");
-                        comment_check =true;
-                    }
-                    if((""+nextline.charAt(i)+nextline.charAt(i+1)).equals("*/")){
-                        comment_check =false;
-                    }
-                }
-                if(i< nextline.length()-3){
-                    if(""+nextline.charAt(i)+nextline.charAt(i+1)+nextline.charAt(i+2) == "/**" ){
-                        comment_check = true;
-                        comment_count++;
-                    }
-                }
-
-                // Avant de close il faut appeler l'analyser pour lui transmettre les stats GP
+            if((nextline.contains("//") && !comment_check) || ((nextline.contains("/*") || nextline.contains("/**")) && nextline.contains("*/"))){
+                comment_count++;
             }
+
+            if((nextline.contains("/*") || nextline.contains("/**")) && !comment_check){
+                comment_check = true;
+            }
+
+
+            System.out.println(comment_check);
+
+            if(nextline.contains("*/") && comment_check){
+               comment_check = false;
+               comment_count++;
+            }
+
+
             if(comment_check){
                 comment_count++;
             }
