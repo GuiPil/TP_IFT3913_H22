73a84a05e4bfc4aac830b2b83dabe951fd0849e3
jaytouz <tousijer@gmail.com>
2022-02-11 00:17:05 -0500

    javadoc in FactoryNode

--- TP1/src/src/FactoryNode.java
+++ TP1/src/src/FactoryNode.java
@@ -1,7 +1,15 @@
 import java.io.File;
 
+/**
+ * Factory Node
+ */
 public class FactoryNode {
 
+    /**
+     * Create TreeMetrics Node based on file
+     * @param f File used to create a node.
+     * @return CodeFile if f is a File, else returns a Package instance.
+     */
     public Node create(File f) {
         if (f.isFile()) {
             return new CodeFile(f);
