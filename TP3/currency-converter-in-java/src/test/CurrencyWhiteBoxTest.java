2c08276193a3fb299ca37249cf6accaac28d5c52
jaytouz <tousijer@gmail.com>
2022-04-08 19:49:42 -0400

    small fix typo

--- TP3/currency-converter-in-java/src/test/CurrencyWhiteBoxTest.java
+++ TP3/currency-converter-in-java/src/test/CurrencyWhiteBoxTest.java
@@ -6,12 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyWhiteBoxTest {
-
     @Test
     public void whiteBoxCouvertureIns(){
         assertEquals(79, Currency.convert(100d, 0.79));
         assertEquals(1.07, Currency.convert(0.85, 1.26));
     }
-
-
 }
