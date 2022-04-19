39d2269ff318890a6abacff22d9b263bddc92416
jaytouz <tousijer@gmail.com>
2022-04-19 15:39:23 -0400

    tache 1 2 3"

--- TP4/proto/main.py
+++ TP4/proto/main.py
@@ -3,6 +3,8 @@ import subprocess
 import os
 import shutil
 from tqdm import tqdm  # dependencies
+import argparse
+import pandas as pd
 
 PATH_TO_REPO = './cloned_repo'
 
@@ -59,8 +61,10 @@ def reset_git(id_commit):
 
 def read_ids(name_main_branch='main'):
     list_ids = []
+    os.chdir(PATH_TO_REPO)
     results = subprocess.run(['git', 'rev-list', name_main_branch], stdout=subprocess.PIPE).stdout.decode('utf-8')
     list_ids = results.split('\n')
+    os.chdir('..')
     return list_ids[:-1]
 
 
@@ -70,8 +74,8 @@ def clone_repo(url):
     :param url: Url du repository
     :return: True si la commande git a fonctionné, False sinon
     """
-    clone = subprocess.run(['git', 'clone', url, './cloned_repo'], stdout=subprocess.PIPE)
-    if os.path.isdir('./cloned_repo'):
+    clone = subprocess.run(['git', 'clone', url, PATH_TO_REPO], stdout=subprocess.PIPE)
+    if os.path.isdir(PATH_TO_REPO):
         print(clone.stdout.decode('utf-8'))
         return True
     else:
@@ -89,60 +93,55 @@ def clean_repo():
         shutil.rmtree(PATH_TO_REPO)  # delete all directory if it already exist
 
 
-def fetch_and_save_id_nc(url):
+def fetch_and_save_id_nc(url, branch_name, file_ext, out_csv):
     print(f'calling, {url}')  # Press Ctrl+F8 to toggle the breakpoint.
     if not clone_repo(url):
         raise ValueError(f"Could not clone the repository of {url}")
-    list_ids = read_ids('main')
+    list_ids = read_ids(branch_name)
     nc_list = []
     mWMC_list = []
     mBC_list = []
-    for id_commit in tqdm(list_ids):
+    for id_commit in tqdm(list_ids[:10]):
         reset_git(id_commit)
-        nc_current = count_class(id_commit, 'py')
+        nc_current = count_class(id_commit, file_ext)
         nc_list.append(nc_current)
         # CALL METRIC ANALYSER
-        # mWMC, mBC = metric_analyser('TP1.jar', PATH_TO_REPO, '.java', './output_analyser.csv')
-        # mWMC_list.append(mWMC)
-        # mBC_list.append(mWMC)
-    save_to_csv(['commit_id', 'nc_list'], list_ids, nc_list) # INUTILE
+        mWMC, mBC = metric_analyser('TP1.jar', PATH_TO_REPO, f'.{file_ext}', out_csv)
+        mWMC_list.append(mWMC)
+        mBC_list.append(mBC)
+    save_to_csv(['commit_id', 'nc_list', 'mWMC', 'mBC'], list_ids, nc_list, mWMC_list, mBC_list) # INUTILE
     clean_repo()
+
 
 def metric_analyser(path_to_tp1, path_to_analyse, file_ext, output_path):
     # java -jar TP1.jar path/to/project/to/analyse .java output/path/for/csv/
-
     mean_WMC, mean_BC = 0, 0
-
-    subprocess.run(["java", "-jar", path_to_tp1, path_to_analyse, file_ext, output_path])
-    fetch_mWMC(output_path)
-    fetch_mBC(output_path)
+    stdout = subprocess.run(["java", "-jar", path_to_tp1, path_to_analyse, file_ext, output_path], stdout=subprocess.PIPE)
+    df = pd.read_csv(f'{output_path}/classes.csv')
     clean_metric_analyse_output(output_path)
-    return mean_WMC, mean_BC
-
-def fetch_mWMC(output_path):
-    pass
+    return df.WMC.mean(), df.classe_BC.mean()
 
 
-def fetch_mBC(output_path):
-    pass
+def clean_metric_analyse_output(output_path):
+    if os.path.isdir(output_path):
+        print(f'removing {output_path} and all subdirectories\n')
+        shutil.rmtree(output_path)  # delete all directory if it already exist
 
-def clean_metric_analyse_output():
-    pass
 
-def main(url):
+def main(url, branch_name, file_ext, out_csv):
     """Entry point of proto"""
     print('fetching data')
-    fetch_and_save_id_nc(url)  # tache 1
+    fetch_and_save_id_nc(url, branch_name, file_ext, out_csv)  # tache 1
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    args = sys.argv
-    if (len(args) == 1):
-        raise ValueError("Missing arguments...")
-    if (len(args) > 2):
-        raise ValueError("To many arguments...")
-
-    main(args[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help='url of the repository to analyse', default='https://github.com/jfree/jfreechart')
+    parser.add_argument('--branch-name', help='Name of the main branch, usually main or master', default='master')
+    parser.add_argument('--type', help='File extension to look for in the analysis', default='java')
+    parser.add_argument('--out', help='url of the repository to analyse', default='./output_analyse/')
+    args = parser.parse_args()
+    main(url=args.url, branch_name=args.branch_name, file_ext=args.type, out_csv=args.out)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
